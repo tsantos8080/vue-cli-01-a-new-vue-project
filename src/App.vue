ade084dc301ff211f569e89e57c3789b40af5e57
tsantos8080 <tsantos8080@gmail.com>
2021-03-21 20:54:48 -0300

    implementada adicao de contatos

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <section>
     <header><h1>My Friends</h1></header>
+    <section>
+    <ul>
+      <li>
+        <new-friend @add-contact="addContact"></new-friend>
+      </li>
+    </ul>
+    </section>
     <ul>
       <friend-contact
         v-for="{ id, name, phone, email, isFavorite } in friends"
@@ -17,9 +24,7 @@
 </template>
 
 <script>
-import FriendContact from "./components/FriendContact.vue";
 export default {
-  components: { FriendContact },
   data() {
     return {
       friends: [
@@ -44,6 +49,16 @@ export default {
     toggleFavoriteStatus(id) {
       const identifiedFriend = this.friends.find((friend) => friend.id === id);
       identifiedFriend.isFavorite = !identifiedFriend.isFavorite;
+    },
+    addContact(name, phone, email) {
+      const newFriendContact = {
+        id: new Date().toISOString(),
+        name,
+        phone,
+        email,
+        isFavorite: false,
+      };
+      this.friends.push(newFriendContact);
     },
   },
 };
@@ -112,5 +127,19 @@ header {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
 }
 </style>
---
+++ src/components/NewFriend.vue
@@ -0,0 +1,54 @@
+<template>
+  <form @submit.prevent="submitData">
+    <div>
+      <label>Name</label>
+      <input type="text" placeholder="Name" v-model="enteredName" />
+    </div>
+    <div>
+      <label>Phone</label>
+      <input type="tel" placeholder="Phone" v-model="enteredPhone" />
+    </div>
+    <div>
+      <label>Email</label>
+      <input type="email" placeholder="Email" v-model="enteredEmail" />
+    </div>
+    <!-- <div>
+      <input type="checkbox" name="isFavorite" v-model="enteredIsFavorite" />
+      <label for="isFavorite">Favorite friend</label>
+    </div> -->
+    <div>
+      <button>Add contact</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: "",
+      enteredName: "",
+      enteredPhone: "",
+      enteredEmail: "",
+      //   enteredIsFavorite: false,
+    };
+  },
+  emits: ["add-contact"],
+  methods: {
+    submitData() {
+      this.$emit(
+        "add-contact",
+        this.enteredName,
+        this.enteredPhone,
+        this.enteredEmail
+      );
+
+      this.id = "";
+      this.enteredName = "";
+      this.enteredPhone = "";
+      this.enteredEmail = "";
+      //   this.isFavorite = false;
+    },
+  },
+};
+</script>
