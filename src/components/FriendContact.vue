93c9cdd0e5c8f9caee634d0d363b51c0a512528f
tsantos8080 <tsantos8080@gmail.com>
2021-03-21 16:26:34 -0300

    testando componentes e estilos

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,33 @@
+<template>
+  <section>
+    <li>
+      <h2>{{ friend.name }}</h2>
+      <button @click="toggleDetails">Show details</button>
+      <ul v-if="detailsAreVisible">
+        <li><strong>Phone:</strong> {{ friend.phone }}</li>
+        <li><strong>Email:</strong> {{ friend.email }}</li>
+      </ul>
+    </li>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "0123 45678 90",
+        email: "manuel@localhost.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
